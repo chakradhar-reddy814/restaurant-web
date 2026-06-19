900fe4d407a6349beccd3a78690e7c9734a03f86
Chakradhar Reddy <chakradhar-reddy814@users.noreply.github.com>
2026-06-19 22:47:57 +0530

    fix: restore clean document layout

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="google-site-verification" content="Y2MdpuuD30jBZ3dwfY_ta3C3TXfS-6STroymx4C5HuI" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
